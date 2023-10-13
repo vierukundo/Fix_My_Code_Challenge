225b0b95ca8aecaec33cefe764d6330db99760c6
vierukundo <vierukundo@gmail.com>
2023-10-13 04:46:57 -0700

    capitalized the class name

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Definition of class"""
 
-class square():
-    
+
+class Square:
+    """class definition"""
+
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initialozation of class instance"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +18,17 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
+        """Perimrter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """string representation"""
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
