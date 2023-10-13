4552dcb7976209d9524bc71c23dcf90d455c4000
vierukundo <vierukundo@gmail.com>
2023-10-13 04:28:57 -0700

    .

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-"""Definition of square class"""
 
+class square:
+    width = 0
+    height = 0
 
-class Square():
-    """class definition"""
     def __init__(self, *args, **kwargs):
-        """instance initialization"""
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if args:
+            if len(args) == 1:
+                self.width = args[0]
+                self.height = args[0]
+            elif len(args) == 2:
+                self.width = args[0]
+                self.height = args[1]
+            else:
+                raise ValueError("Invalid number of positional arguments")
+        elif kwargs:
+            self.width = kwargs.get("width", self.width)
+            self.height = kwargs.get("height", self.height)
 
     def area_of_my_square(self):
-        """ Area of the square """
+        """Area of the square"""
         return self.width * self.width
 
     def PermiterOfMySquare(self):
@@ -19,10 +28,8 @@ class Square():
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
